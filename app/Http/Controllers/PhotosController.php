80dc4fd008d40ef5610752e7453417f532fe2f1b
Teodor Savin <teodorsavin@gmail.com>
2018-12-23 18:25:33 +0100

    create category

--- app/Http/Controllers/PhotosController.php
+++ app/Http/Controllers/PhotosController.php
@@ -7,26 +7,6 @@ use App\Category;
 
 class PhotosController extends Controller
 {
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-        $this->middleware('auth');
-    }
-
-    /**
-     * Show the application dashboard.
-     *
-     * @return \Illuminate\Contracts\Support\Renderable
-     */
-    public function index()
-    {
-        return view('home');
-    }
-
     public function categories()
     {
         return Category::all();
--- routes/api.php
+++ routes/api.php
@@ -17,4 +17,4 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('categories', 'PhotosController@categories');
+Route::get('/categories', 'PhotosController@categories');
