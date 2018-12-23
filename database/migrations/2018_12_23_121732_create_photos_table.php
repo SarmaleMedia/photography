2ae6fc9ab4a019b9992766c401ba0a72191bc5a7
Teodor Savin <teodorsavin@gmail.com>
2018-12-23 18:08:45 +0100

    create category

---
+++ database/migrations/2018_12_23_121732_create_photos_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePhotosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('photos', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('photo_slug', 255)->nullable();
+            $table->string('photo_file', 255);
+            $table->timestamp('photo_date');
+            $table->string('photo_location', 255)->nullable();
+            $table->integer('user_id')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('photos');
+    }
+}
