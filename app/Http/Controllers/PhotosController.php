0f362ed8182b3c67127487052e418bd9cef32a2e
Teodor Savin <t.savin@global.leaseweb.com>
2018-12-29 21:41:51 +0100

    guzzle

--- app/Http/Controllers/PhotosController.php
+++ app/Http/Controllers/PhotosController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Http\Requests\PhotoFormRequest;
+use Aws\S3\MultipartUploader;
+use Illuminate\Support\Facades\Storage;
 use App\Category;
 
 class PhotosController extends Controller
@@ -10,5 +12,25 @@ class PhotosController extends Controller
     public function categories()
     {
         return Category::all();
+    }
+
+    public function uploadPhoto(PhotoFormRequest $request)
+    {
+        $disk = Storage::disk('s3');
+        $file = $request->files->get('file');
+
+        if (!empty($file)) {
+            $uploader = new MultipartUploader($disk->getDriver()->getAdapter()->getClient(), $file->getPathName(), [
+                'bucket' => env('AWS_BUCKET'),
+                'key'    => rand() . ' - ' . $file->getClientOriginalName(),
+                'acl'    => 'public-read'
+            ]);
+
+            $result = $uploader->upload();
+
+            echo '<pre>';
+            print_r($result->get('ObjectURL'));
+            die();
+        }
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -18,3 +18,4 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 });
 
 Route::get('/categories', 'PhotosController@categories');
+Route::post('/photo', 'PhotosController@uploadPhoto');
